6a828c913c8374f0b7fb2e5a4b3c927d931b122b
Rasmus Krämer <git@rfk.io>
2024-11-10 15:14:51 +0100

    Initial Commit

---
+++ SatelliteGuard/SatelliteGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliteGuardApp.swift
+//  SatelliteGuard
+//
+//  Created by Rasmus Krämer on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SatelliteGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
