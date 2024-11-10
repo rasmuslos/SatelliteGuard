ae803906f1dc1f617df654a793617f29a783c041
Rasmus Krämer <git@rfk.io>
2024-11-10 16:53:33 +0100

    Add WireguardKit

---
+++ Wireguard/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WireGuard",
+    platforms: [
+        .tvOS(.v17),
+    ],
+    products: [
+        .library(name: "WireGuardKit", targets: ["WireGuardKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "WireGuardKit", dependencies: ["WireGuardKitGo", "WireGuardKitC"]),
+        .target(name: "WireGuardKitC", dependencies: [], publicHeadersPath: "."),
+        .target(name: "WireGuardKitGo", dependencies: [], exclude: ["goruntime-boottime-over-monotonic.diff", "go.mod", "go.sum", "api-apple.go", "Makefile"], publicHeadersPath: ".", linkerSettings: [.linkedLibrary("wg-go")])
+    ]
+)
